47d2146758bf780ede6726e92d4f8c98c841fd44
Naseem <naseemhamoud@gmail.com>
2016-02-04 21:36:28 -0800

    Built main page and transitions to exercises workout and credits

--- Workoutya/app/src/main/java/com/example/justin/workoutya/MainMenu.java
+++ Workoutya/app/src/main/java/com/example/justin/workoutya/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.justin.workoutya;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button exerciseBtn = (Button)findViewById(R.id.exercise_button);
+        Button workoutBtn = (Button)findViewById(R.id.workout_button);
+        Button creditsBtn = (Button)findViewById(R.id.credits_button);
+
+
+        exerciseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, Exercise.class));
+            }
+        });
+
+        workoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, Workout.class));
+            }
+        });
+
+        creditsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, Credits.class));
+            }
+        });
+
     }
+
+
 }
