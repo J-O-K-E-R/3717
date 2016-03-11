313d9bb87262271920056e693b80c8f51ef1b264
Daniel Zhang <danielvmaple@gmail.com>
2016-03-11 01:46:50 -0800

    added youtube video

---
+++ Workoutya/app/src/main/java/com/example/justin/workoutya/Config.java
@@ -0,0 +1,11 @@
+package com.example.justin.workoutya;
+
+/**
+ * Created by Daniel Zhang on 2016/3/11.
+ */
+public class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyB4GNVHrQO58UyuDiV4J97hhzc6C7sW9KQ";
+}
